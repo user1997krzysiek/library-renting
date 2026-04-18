e41ff2eebc41d38f1b0cb9757f45f38920888e0f
Krzysiek <krzysztof.orawski@wp.pl>
2026-04-18 11:34:39 +0200

    add loop

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,14 @@ public class Main {
     public static void main (String[] args) {
         List<Book> list = new ArrayList<>();
         list.add(new Book ("Pride and Prejudice" , 50, "rack number 10"));
-        Book element = list.get(0);
-        System.out.println(element);
+        list.add(new Book("Pan tadeusz", 100, "rack number 5"));
+//        Book element = list.get(0);
+//        Book element1 = list.get(1);
+//        System.out.println(element);
+//        System.out.println(element1);
+        for(int i = 0; i < list.size();i++) {
+            Book element = list.get(i);
+            System.out.println(element);
+        }
     }
 }
