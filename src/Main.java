355ea4702aca5487643aa9815b4d0face7a65380
Krzysiek <krzysztof.orawski@wp.pl>
2026-04-25 13:40:31 +0200

    add scanner

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,30 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         List<Book> list = new ArrayList<>();
-        list.add(new Book ("Pride and Prejudice" , 50, "rack number 10"));
+        list.add(new Book("Pride and Prejudice", 50, "rack number 10"));
         list.add(new Book("Pan tadeusz", 100, "rack number 5"));
 //        Book element = list.get(0);
 //        Book element1 = list.get(1);
 //        System.out.println(element);
 //        System.out.println(element1);
-        for(int i = 0; i < list.size();i++) {
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+
+
+
+        for (int i = 1; i <= number; i++) {
+            list.add(new Book("Dziady cz " + i, 100, "rack number  " + i + 4));
+
+        }
+
+        for (int i = 0; i < list.size(); i++) {
             Book element = list.get(i);
             System.out.println(element);
+
         }
     }
 }
